fc0c16761cc4ccd3bd9514a5dc776f42d9386763
SSinist3r <frovi.ga@gmail.com>
2022-07-20 22:51:43 +0530

    initial commit

---
+++ VanillaSSinn3r_CS/VanillaSSinn3r/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using System.Drawing;
+
+namespace VanillaSSinn3r
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.namePlateRangeSlider = new System.Windows.Forms.TrackBar();
+            this.processBox = new System.Windows.Forms.ComboBox();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.attachBtn = new System.Windows.Forms.Button();
+            this.infoBox = new System.Windows.Forms.RichTextBox();
+            this.fovLabel = new System.Windows.Forms.Label();
+            this.fovTextBox = new System.Windows.Forms.TextBox();
+            this.fovSetBtn = new System.Windows.Forms.Button();
+            this.namePlateCheckBox = new System.Windows.Forms.CheckBox();
+            this.fovCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.namePlateRangeSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // namePlateRangeSlider
+            // 
+            this.namePlateRangeSlider.Location = new System.Drawing.Point(137, 212);
+            this.namePlateRangeSlider.Maximum = 10000;
+            this.namePlateRangeSlider.Name = "namePlateRangeSlider";
+            this.namePlateRangeSlider.Size = new System.Drawing.Size(260, 45);
+            this.namePlateRangeSlider.TabIndex = 0;
+            this.namePlateRangeSlider.Value = 20;
+            this.namePlateRangeSlider.Scroll += new System.EventHandler(this.namePlateRangeSlider_Scroll);
+            // 
+            // processBox
+            // 
+            this.processBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.processBox.FormattingEnabled = true;
+            this.processBox.Location = new System.Drawing.Point(12, 178);
+            this.processBox.Name = "processBox";
+            this.processBox.Size = new System.Drawing.Size(223, 28);
+            this.processBox.TabIndex = 1;
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.AutoSize = true;
+            this.refreshBtn.Location = new System.Drawing.Point(242, 178);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(76, 30);
+            this.refreshBtn.TabIndex = 2;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // attachBtn
+            // 
+            this.attachBtn.AutoSize = true;
+            this.attachBtn.Location = new System.Drawing.Point(324, 178);
+            this.attachBtn.Name = "attachBtn";
+            this.attachBtn.Size = new System.Drawing.Size(75, 30);
+            this.attachBtn.TabIndex = 3;
+            this.attachBtn.Text = "Attach";
+            this.attachBtn.UseVisualStyleBackColor = true;
+            this.attachBtn.Click += new System.EventHandler(this.attachBtn_Click);
+            // 
+            // infoBox
+            // 
+            this.infoBox.BackColor = System.Drawing.Color.Black;
+            this.infoBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.infoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoBox.ForeColor = System.Drawing.Color.Chartreuse;
+            this.infoBox.HideSelection = false;
+            this.infoBox.Location = new System.Drawing.Point(12, 12);
+            this.infoBox.Name = "infoBox";
+            this.infoBox.ReadOnly = true;
+            this.infoBox.Size = new System.Drawing.Size(387, 160);
+            this.infoBox.TabIndex = 22;
+            this.infoBox.Text = "";
+            // 
+            // fovLabel
+            // 
+            this.fovLabel.AutoSize = true;
+            this.fovLabel.Location = new System.Drawing.Point(12, 261);
+            this.fovLabel.Name = "fovLabel";
+            this.fovLabel.Size = new System.Drawing.Size(396, 13);
+            this.fovLabel.TabIndex = 23;
+            this.fovLabel.Text = "FOV Set Value :                                                                  " +
+    "           (Default - 1.5708)";
+            // 
+            // fovTextBox
+            // 
+            this.fovTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.fovTextBox.Location = new System.Drawing.Point(137, 255);
+            this.fovTextBox.Name = "fovTextBox";
+            this.fovTextBox.Size = new System.Drawing.Size(98, 26);
+            this.fovTextBox.TabIndex = 24;
+            this.fovTextBox.Text = "1.5708";
+            // 
+            // fovSetBtn
+            // 
+            this.fovSetBtn.AutoSize = true;
+            this.fovSetBtn.Location = new System.Drawing.Point(242, 254);
+            this.fovSetBtn.Name = "fovSetBtn";
+            this.fovSetBtn.Size = new System.Drawing.Size(76, 30);
+            this.fovSetBtn.TabIndex = 25;
+            this.fovSetBtn.Text = "Set";
+            this.fovSetBtn.UseVisualStyleBackColor = true;
+            this.fovSetBtn.Click += new System.EventHandler(this.fovSetBtn_Click);
+            // 
+            // namePlateCheckBox
+            // 
+            this.namePlateCheckBox.AutoSize = true;
+            this.namePlateCheckBox.Location = new System.Drawing.Point(12, 219);
+            this.namePlateCheckBox.Name = "namePlateCheckBox";
+            this.namePlateCheckBox.Size = new System.Drawing.Size(119, 17);
+            this.namePlateCheckBox.TabIndex = 26;
+            this.namePlateCheckBox.Text = "NamePlate Range: ";
+            this.namePlateCheckBox.UseVisualStyleBackColor = true;
+            this.namePlateCheckBox.CheckedChanged += new System.EventHandler(this.namePlateCheckBox_CheckedChanged);
+            // 
+            // fovCheckBox
+            // 
+            this.fovCheckBox.AutoSize = true;
+            this.fovCheckBox.Location = new System.Drawing.Point(12, 259);
+            this.fovCheckBox.Name = "fovCheckBox";
+            this.fovCheckBox.Size = new System.Drawing.Size(105, 17);
+            this.fovCheckBox.TabIndex = 27;
+            this.fovCheckBox.Text = "FOV Set Value : ";
+            this.fovCheckBox.UseVisualStyleBackColor = true;
+            this.fovCheckBox.CheckedChanged += new System.EventHandler(this.fovCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 299);
+            this.Controls.Add(this.fovCheckBox);
+            this.Controls.Add(this.namePlateCheckBox);
+            this.Controls.Add(this.fovSetBtn);
+            this.Controls.Add(this.fovTextBox);
+            this.Controls.Add(this.fovLabel);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.attachBtn);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.processBox);
+            this.Controls.Add(this.namePlateRangeSlider);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.namePlateRangeSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar namePlateRangeSlider;
+        private System.Windows.Forms.ComboBox processBox;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.Button attachBtn;
+        private System.Windows.Forms.RichTextBox infoBox;
+        private System.Windows.Forms.Label fovLabel;
+        private System.Windows.Forms.TextBox fovTextBox;
+        private System.Windows.Forms.Button fovSetBtn;
+        private System.Windows.Forms.CheckBox namePlateCheckBox;
+        private System.Windows.Forms.CheckBox fovCheckBox;
+    }
+}
+
---
+++ VanillaSSinn3r_CS/VanillaSSinn3r/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management;
+using Binarysharp.MemoryManagement;
+using System.Diagnostics;
+using VanillaSSinn3r.Properties;
+
+namespace VanillaSSinn3r
+{
+	public partial class Form1 : Form
+	{
+		public static Process[] Proc;
+
+		public static MemorySharp Sharp;
+
+		public static bool Attached;
+
+		public static bool AppClosing;
+
+		public static IntPtr RangeAddy;
+
+		public static IntPtr FOVAddy;
+
+		public static int SelectedProcess;
+
+		public static string Version;
+
+		public Form1()
+		{
+			InitializeComponent();
+			Text = "WoW Vanilla/TBC Tool By SSinist3r";
+			Load += new EventHandler(Form1_Load);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+			ProcessWatcherInit();
+			Init();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (!Form1.Attached)
+			{
+				return;
+			}
+			Form1.AppClosing = true;
+			if(namePlateCheckBox.Checked)
+				Settings.Default.DefaultRange = (int)namePlateRangeSlider.Value;
+			if(fovCheckBox.Checked)
+				Settings.Default.DefaultFOV = float.Parse(fovTextBox.Text);
+			Settings.Default.DefaultRangeBool = namePlateCheckBox.Checked;
+			Settings.Default.DefaultFOVBool = fovCheckBox.Checked;
+			Settings.Default.Save();
+			Form1.Sharp.Write<float>(Form1.RangeAddy, 400f, false);
+			Form1.Sharp.Write<float>(Form1.FOVAddy, 1.5708f, false);
+		}
+
+		private void Init()
+		{
+			processBox.Invoke(new MethodInvoker(delegate ()
+			{
+				processBox.Items.Clear();
+			}));
+			Form1.Proc = Process.GetProcessesByName("Wow");
+			if (Form1.Proc.Length == 0)
+			{
+				return;
+			}
+			Process[] proc = Form1.Proc;
+			for (int i = 0; i < proc.Length; i++)
+			{
+				Process instance = proc[i];
+				processBox.Invoke(new MethodInvoker(delegate ()
+				{
+					processBox.Items.Add("World of Warcraft [" + instance.Id + "]");
+				}));
+			}
+			processBox.Invoke(new MethodInvoker(delegate ()
+			{
+				processBox.SelectedIndex = 0;
+			}));
+		}
+
+		public void ProcessWatcherInit()
+		{
+			ManagementEventWatcher managementEventWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+			managementEventWatcher.EventArrived += OnProcessStart;
+			managementEventWatcher.Start();
+			ManagementEventWatcher managementEventWatcher2 = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+			managementEventWatcher2.EventArrived += OnProcessStop;
+			managementEventWatcher2.Start();
+		}
+
+		public void OnProcessStart(object sender, EventArrivedEventArgs e)
+		{
+			if (e.NewEvent.Properties["ProcessName"].Value.ToString().ToLower() == "wow.exe")
+			{
+				Init();
+			}
+		}
+
+		public void OnProcessStop(object sender, EventArrivedEventArgs e)
+		{
+			string text = e.NewEvent.Properties["ProcessName"].Value.ToString();
+			int num = int.Parse(e.NewEvent.Properties["ProcessID"].Value.ToString());
+			if (text.ToLower() == "wow.exe")
+			{
+				Init();
+			}
+			if (!Form1.Attached || num != Form1.SelectedProcess)
+			{
+				return;
+			}
+			Print("WoW closed detaching.");
+			Form1.Sharp.Handle.Close();
+			Form1.Sharp.Dispose();
+			Form1.Attached = false;
+		}
+
+		public void Print(string msg)
+		{
+			infoBox.Invoke(new MethodInvoker(delegate ()
+			{
+				infoBox.AppendText(DateTime.Now.ToString("[hh:mm:ss tt]   ") + msg + Environment.NewLine);
+			}));
+		}
+
+		public void infoPrint(string forText, float value)
+		{
+			if(forText.Contains("nameplate"))
+				Print("Nameplate Range: " + (int)((value / 100)*2.75) + " yards");
+			else if (forText.Contains("fov"))
+				Print("FOV: " + value + "	(Use In Game: \' /console ReloadUI \')");
+		}
+
+
+		private void namePlateRangeSlider_Scroll(object sender, EventArgs e)
+        {
+			if (!Form1.Attached || !namePlateCheckBox.Checked)
+			{
+				return;
+			}
+			Form1.Sharp.Write<float>(Form1.RangeAddy, (float)namePlateRangeSlider.Value, false);
+			if (!Form1.AppClosing)
+			{
+				Settings.Default.DefaultRange = namePlateRangeSlider.Value;
+				infoPrint("nameplate", namePlateRangeSlider.Value);
+			}
+			Settings.Default.Save();
+		}
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+			Init();
+		}
+
+        private void attachBtn_Click(object sender, EventArgs e)
+        {
+			if (Form1.Attached)
+			{
+				Print("We are already attached to WoW");
+				return;
+			}
+			if (processBox.SelectedIndex == -1)
+			{
+				Print("You must select a valid instance of WoW to attach");
+				return;
+			}
+			try
+			{
+				Form1.SelectedProcess = int.Parse(GetSubstringByString("[", "]", processBox.SelectedItem.ToString()));
+				Form1.Version = GetFileVerion(Form1.SelectedProcess);
+				Print("Attaching to WoW with id [" + Form1.SelectedProcess + "]...");
+				Form1.Sharp = new MemorySharp(Form1.SelectedProcess);
+				Form1.Attached = true;
+				Print("Successfully attached to WoW [" + Form1.SelectedProcess + "]");
+				if (Form1.Version.Contains("5875")) // Vanilla
+				{
+					Print("World of Warcraft [" + Form1.Version + "] detected!");
+					Form1.RangeAddy = new IntPtr(12900744); // 0xC4D988
+					Form1.FOVAddy = new IntPtr(8423860); // 0x8089B4
+				}
+				else if (Form1.Version.Contains("8606")) // TBC
+				{
+					Print("World of Warcraft [" + Form1.Version + "] detected!");
+					Form1.RangeAddy = new IntPtr(12209040); // 0xBA4B90
+					Form1.FOVAddy = new IntPtr(9132548); // 0x8b5a04
+				}
+				else if (Form1.Version.Contains("12340")) // Wotlk
+				{
+					Print("World of Warcraft [" + Form1.Version + "] detected!");
+					Form1.RangeAddy = new IntPtr(12209040); // 0xBA4B90 // still need to find this
+					Form1.FOVAddy = new IntPtr(10390920); // 0x9e8d88
+				}
+				else
+				{
+					Print("World of Warcraft [" + Form1.Version + "] is not supported.");
+					Form1.Sharp.Handle.Close();
+					Form1.Sharp.Dispose();
+					Form1.Attached = false;
+					Print("Detached from WoW [" + Form1.SelectedProcess + "]");
+				}
+				if (Form1.Attached)
+				{
+					namePlateCheckBox.Checked = Settings.Default.DefaultRangeBool;
+					fovCheckBox.Checked = Settings.Default.DefaultFOVBool;
+					if (namePlateCheckBox.Checked)
+                    {
+						Form1.Sharp.Write<float>(Form1.RangeAddy, (float)Settings.Default.DefaultRange, false);
+						namePlateRangeSlider.Value = (int)Form1.Sharp.Read<float>(Form1.RangeAddy, false);
+						infoPrint("nameplate", namePlateRangeSlider.Value);
+
+					}
+					if(fovCheckBox.Checked)
+                    {
+						Form1.Sharp.Write<float>(Form1.FOVAddy, (float)Settings.Default.DefaultFOV, false);
+						fovTextBox.Text = (float)Form1.Sharp.Read<float>(Form1.FOVAddy, false) + "";
+						infoPrint("fov", float.Parse(fovTextBox.Text));
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Print(ex.Message);
+			}
+		}
+
+		public string GetFileVerion(int procId)
+		{
+			return Process.GetProcessById(procId).MainModule.FileVersionInfo.FileVersion;
+		}
+
+		public string GetSubstringByString(string a, string b, string c)
+		{
+			return c.Substring(c.IndexOf(a, StringComparison.Ordinal) + a.Length, c.IndexOf(b, StringComparison.Ordinal) - c.IndexOf(a, StringComparison.Ordinal) - a.Length);
+		}
+
+        private void fovSetBtn_Click(object sender, EventArgs e)
+        {
+			if (!Form1.Attached || !fovCheckBox.Checked)
+			{
+				return;
+			}
+			Form1.Sharp.Write<float>(Form1.FOVAddy, float.Parse(fovTextBox.Text), false);
+			if (!Form1.AppClosing)
+			{
+				if(fovTextBox.Text != null)
+                {
+					Settings.Default.DefaultFOV = float.Parse(fovTextBox.Text);
+					infoPrint("fov", float.Parse(fovTextBox.Text));
+				}
+			}
+			Settings.Default.Save();
+		}
+
+        private void namePlateCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+			Settings.Default.DefaultRangeBool = namePlateCheckBox.Checked;
+			Settings.Default.Save();
+			namePlateRangeSlider.Value = Settings.Default.DefaultRange;
+		}
+
+        private void fovCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+			Settings.Default.DefaultFOVBool = fovCheckBox.Checked;
+			Settings.Default.Save();
+			fovTextBox.Text = Settings.Default.DefaultFOV + "";
+		}
+    }
+}
